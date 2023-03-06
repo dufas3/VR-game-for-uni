257c12ba15fd120498f8d1bdd2283f557316c157
Skirmantas <skirma22@gmail.com>
2023-03-06 14:53:45 +0200

    first commit

---
+++ SteamVR Emergent game/Assets/Scripts/Furnace.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Furnace : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Rectrash")
+        {
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.tag == "Grewaste") 
+        {
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.tag == "Genwaste")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+}
---
+++ SteamVR Emergent game/Assets/Scripts/SpawnTrash.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrash : MonoBehaviour
+{
+    public GameObject GenTrash;
+    public GameObject RecTrash;
+    public GameObject GreTrash;
+
+    void Start()
+    {
+        StartCoroutine(randomGentrash());
+        StartCoroutine(randomGretrash());
+        StartCoroutine(randomRectrash());
+    }
+
+    private IEnumerator randomGentrash()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(0, 10));
+            Instantiate(GenTrash, transform.position, transform.rotation);
+        }
+    }
+    private IEnumerator randomRectrash()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(0, 10));
+            Instantiate(RecTrash, transform.position, transform.rotation);
+        }
+    }
+    private IEnumerator randomGretrash()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(0, 10));
+            Instantiate(GreTrash, transform.position, transform.rotation);
+        }
+    }
+}
